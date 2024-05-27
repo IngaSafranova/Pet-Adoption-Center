f424ffe160d6b2f397ccc746205429d0c768d5ba
ingasafranova <sfrnv@yahoo.co.uk>
2024-05-27 09:38:26 +0100

    blured circles added

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,16 +7,17 @@ module.exports = {
         midnight: "#001D37",
         pink: "#D660FF",
         blue: "#475AFF",
+        circleBlue: "#6079FF",
       },
       maxWidth: {
-        '8xl': '1440px'
+        "8xl": "1440px",
       },
       fontFamily: {
-        default : ['Source Sans 3', 'san-serif']
+        default: ["Source Sans 3", "san-serif"],
       },
       fontSize: {
-        '8.5xl': '6.25rem'
-      }
+        "8.5xl": "6.25rem",
+      },
     },
   },
   plugins: [],
